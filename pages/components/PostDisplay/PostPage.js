b3b4e448174d9994da8de6029413d0ba187472fd
laurenmcp <lmcpeters3@gatech.edu>
2022-11-29 17:39:18 -0500

    fixed problem with endless state update

--- pages/components/PostDisplay/PostPage.js
+++ pages/components/PostDisplay/PostPage.js
@@ -1,31 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EditDelete from "./EditDelete";
 import CommentComponent from "../CommentComponent";
 import CommentForm from "../CommentForm";
 
+
 function PostPage(props) {
     const { post } = props;
-    const [comments, setComments] = useState([])
+    const [comments, setComments] = useState([]);
     const [addComment, setAddComment] = useState({content: ""});
 
-    fetch("http://localhost:3000/api/comments")
+    // fetch("http://localhost:3000/api/comments")
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         var temp = data[post["_id"].toString()];
+    //         setComments(temp);
+    //     })
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/comments")
         .then((res) => res.json())
         .then((data) => {
             var temp = data[post["_id"].toString()];
-            setComments(temp);
-        })
-
-    var commentsCopy = [
-        ...comments
-    ]
-
+            setComments(comments => (temp));
+        });
+    }, [])
+    
     return (
         <div>
             <h1>{post.title}</h1>
             <p>{post.body}</p>
             <EditDelete post={post}></EditDelete>
             <div>
-                {commentsCopy.map((comment) => {
+                {comments.map((comment) => {
                     return <CommentComponent comment={comment}/>;
                 })}
                 <CommentForm fields={addComment} setFields={setAddComment}/>
